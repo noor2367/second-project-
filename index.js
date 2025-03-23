2235b8bf5a9150e1e6cce059bf07d457cd3f3d62
noor2367 <ebdevelopernoor8@gmail.com>
2025-03-23 10:24:00 -0700

    mobile updated

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+    const humbueger = document.querySelector(".mobile-menu")
+    humbueger.style.display="flex";
+    
+}
+function closeSidear(){
+    const humbueger = document.querySelector(".mobile-menu")
+    humbueger.style.display="none";
+}
